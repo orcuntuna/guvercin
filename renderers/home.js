31b021bd0c75c040a883c10cccabda78fa455e29
orcuntuna <turboindirblog@gmail.com>
2020-01-04 14:37:53 +0300

    designed request bar

--- renderers/home.js
+++ renderers/home.js
@@ -1,3 +1,27 @@
+function validateURL(){
+    var url = $("#url").val();
+    if(validate({website: url}, {website: {url: true}}) != undefined){
+        return false;
+    }else{
+        return true;
+    }
+}
+
 $(function(){
-    
+    $("#method-list a").on("click", function(e){
+        e.preventDefault();
+    });
+    $("#request-form").on("submit", function(e){
+        e.preventDefault();
+        if(validateURL()){
+
+        }else{
+            $("#request-alert span.message").html("<strong>Hata!</strong> Girdğiniz istek adresi geçerli bir URL değil.");
+            $("#request-alert").addClass("show");
+        }
+    });
+    $("#request-alert button").on("click", function(e){
+        e.preventDefault();
+        $("#request-alert").removeClass("show");
+    });
 });
