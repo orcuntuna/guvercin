cb066f4eaa37257800975d6ca96e4d99caf0b9aa
orcuntuna <turboindirblog@gmail.com>
2020-01-08 23:14:24 +0300

    settings drawer toggle

--- renderers/home.js
+++ renderers/home.js
@@ -45,6 +45,20 @@ $(function(){
         checkParamsAndAdd();
     });
 
+    $(".open-drawer a").on("click", function(e){
+        e.preventDefault();
+        $("#drawer").show();
+        $(".open-drawer").hide();
+        $(".close-drawer").show();
+    });
+
+    $(".close-drawer a").on("click", function(e){
+        e.preventDefault();
+        $("#drawer").hide();
+        $(".close-drawer").hide();
+        $(".open-drawer").show();
+    });
+
     body_editor = ace.edit("body-editor");
     var JSONMode = require("ace-builds/src/mode-json").Mode;
     body_editor.session.setMode(new JSONMode());
