e6fb90431051b028a9635126a8039e5d7fdb53a7
orcuntuna <turboindirblog@gmail.com>
2020-01-14 16:57:47 +0300

    response editor theme change

--- renderers/home.js
+++ renderers/home.js
@@ -3,3 +3,6 @@ const ace = require('ace-builds/src/ace');
 var json_beautify = require("json-beautify");
 var xml_beautify = require('xml-formatter');
 const fetch = require("node-fetch");
+
+var body_editor;
+var response_editor;
